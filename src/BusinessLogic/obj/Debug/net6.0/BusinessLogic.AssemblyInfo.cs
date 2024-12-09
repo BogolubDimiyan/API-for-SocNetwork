a018a925413f2b44f62e8178111859a8e6c83746
Danilabv <161813147+BogolubDimiyan@users.noreply.github.com>
2024-12-09 12:20:59 +0300

    fix

--- src/BusinessLogic/obj/Debug/net6.0/BusinessLogic.AssemblyInfo.cs
+++ src/BusinessLogic/obj/Debug/net6.0/BusinessLogic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da6f7773f41769f4630656e0fd48a7f7d80d685a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30b766b986faa4dc45d72cfac67ae12b25c558a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
